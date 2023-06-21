d19fdfd56c5768f06e7e46351d607db0fe7ed8f0
ruipedro6 <126692711+ruipedro6@users.noreply.github.com>
2023-06-21 21:00:04 +0100

    CONCLUIDO

---
+++ Assets/ModTheCube/Cube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public MeshRenderer Renderer;
+    
+    void Start()
+    {
+        transform.position = new Vector3(3, 4, 1);
+        transform.localScale = Vector3.one * 2.5f;
+        
+        Material material = Renderer.material;
+        
+        material.color = new Color(0.2f, 2.0f, 0.7f, 0.9f);
+    }
+    
+    void Update()
+    {
+        transform.Rotate(20.0f * Time.deltaTime, 0.0f, 0.0f);
+    }
+}
